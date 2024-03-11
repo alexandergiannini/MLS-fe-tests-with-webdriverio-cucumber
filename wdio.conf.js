3568ddb57bfd42f99ce2f44768b22aea2631576e
Alexander Giannini <alexgiannini@Alexanders-MBP.lan>
2024-03-10 17:50:31 -0700

    testing jenkins config

--- wdio.conf.js
+++ wdio.conf.js
@@ -102,7 +102,7 @@ exports.config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    // services: [],
+    services: ['chromedriver', 'safaridriver'], //ADDED!
     //
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
@@ -125,7 +125,15 @@ exports.config = {
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: https://webdriver.io/docs/dot-reporter
-    reporters: ['spec'],
+    reporters: ['spec',
+    ['junit', {
+        outputDir: 'junit-reports',
+        outputFileFormat: function(options) { // optional
+            //return `results-${new Date().getTime().xml}`
+            return `results-${options.cid}.${options.capabilities}.xml`
+        }
+    }]
+],
 
     // If you are using Cucumber you need to specify the location of your step definitions.
     cucumberOpts: {
